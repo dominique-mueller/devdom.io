623937bc0d53b8a10197baf9dbef5a9b21b007e5
Dominique Müller <dominique.m.mueller@gmail.com>
2021-04-14 00:20:14 +0200

    Migrate to exec-bin

--- run-executable.js
+++
@@ -1,9 +0,0 @@
-import { spawn } from 'child_process';
-import { argv } from 'process';
-
-// Run executable
-spawn(argv[2], argv.slice(3), {
-  stdio: 'inherit',
-}).on('exit', (code) => {
-  process.exit(code);
-});
