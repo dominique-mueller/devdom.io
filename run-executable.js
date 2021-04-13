31519c5895584ae8960de45fa83345baa8112333
Dominique Müller <dominique.m.mueller@gmail.com>
2021-04-13 23:47:50 +0200

    Run hugo as npm script

---
+++ run-executable.js
@@ -0,0 +1,9 @@
+import { spawn } from 'child_process';
+import { argv } from 'process';
+
+// Run executable
+spawn(argv[2], argv.slice(3), {
+  stdio: 'inherit',
+}).on('exit', (code) => {
+  process.exit(code);
+});
